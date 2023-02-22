76a96a15bf9316ef3090bd84f6bffe403fa45a59
LennyGenius <rescue.ajis@gmail.com>
2023-02-22 09:21:46 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - Add two integers
- * x: int type number
- * y: int type number
+ *@x: int type number
+ *@y: int type number
  * Return: result of addition
  */
 int add(int x, int y)
